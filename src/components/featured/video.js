c10cd2d79ab6569400d7b81be61a6587822ccbde
rohankokkulabp <rohan.kokkula@botpress.com>
2022-02-15 23:25:01 +0530

    removed unused code

--- src/components/featured/video.js
+++ src/components/featured/video.js
@@ -1,35 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import dynamic from 'next/dynamic';
 import Image from 'next/image';
-import { useRef, useState } from 'react';
-
-const ReactPlayer = dynamic(() => import('react-player/lazy'), { ssr: false });
-
-const Placeholder = ({ setClickedPlay, setPlaying }) => {
-    return (
-        // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-        // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-        <div
-            className="relative cursor-pointer max-h-[348px]"
-            onClick={() => {
-                setClickedPlay(true);
-                setPlaying(true);
-            }}>
-            <div className="absolute z-10 h-full  w-full flex items-center justify-center">
-                <Image src="/play-btn.png" height={160} width={160} quality={100} />
-            </div>
-
-            <Image src="/placeholder-video-homepage.png" height={348} width={548} quality={80} />
-        </div>
-    );
-};
-
 const Video = () => {
-    const [playing, setPlaying] = useState(false);
-    const [hovering, setHover] = useState(false);
-    const [clickedPlay, setClickedPlay] = useState(false);
-    const vidRef = useRef(null);
-
     return (
         <div className="relative">
             <div className="relative -mt-2 max-w-[548px] max-h-[348px] shadow-lg rounded-lg">
